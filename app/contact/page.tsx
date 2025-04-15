9a1ee01e27657e8f09f2df615be69c44e9b97f55
Joël Mik <contact@joelmik.nl>
2025-04-15 13:40:06 +0200

    some changes to contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,6 @@ import ContactForm from "../components/ContactForm";
 export default function ContactPage() {
   return (
     <div className="container mx-auto py-12">
-      <h1 className="text-3xl font-bold text-center mb-8">Neem Contact Op</h1>
       <ContactForm />
     </div>
   );
